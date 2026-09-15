ff140f908c3db875fb000dd714a4b7f5cbd6938a
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-09-15 19:55:19 +0200

    Update install-packages.sh

--- build_files/install-packages.sh
+++ build_files/install-packages.sh
@@ -126,7 +126,7 @@ GUEST_DESKTOP_AGENTS=(
 dnf -y --setopt=install_weak_deps=False install "${SHARED_PACKAGES[@]}" "${GUEST_DESKTOP_AGENTS[@]}" "${FEDORA_PACKAGES[@]}"
 
 # remove unwanted packages
-readarray -t INSTALLED < <(rpm -qa --queryformat='%{NAME}\n' "${UNVANTED_PACKAGES[@]}" "${BLUETOOTH_PACKAGES[@]}" "${WIFI_PACKAGES[@]}" "${DEVELOPMENT_PACKAGES[@]}" "${QEMU_PACKAGES[@]}" "${OPENSSH_SERVER[@]}" 2>/dev/null || true)
+readarray -t INSTALLED < <(rpm -qa --queryformat='%{NAME}\n'  "${BLUETOOTH_PACKAGES[@]}" "${WIFI_PACKAGES[@]}" "${DEVELOPMENT_PACKAGES[@]}" "${QEMU_PACKAGES[@]}" "${OPENSSH_SERVER[@]}" 2>/dev/null || true)
 if [[ "${#INSTALLED[@]}" -gt 0 ]]; then
     dnf -y remove "${INSTALLED[@]}"
 else
