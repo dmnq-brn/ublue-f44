c1e7a1e1388616ccc5a72d40a59ebd5acd445fdf
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-09-15 19:25:57 +0200

    Update install-packages.sh

--- build_files/install-packages.sh
+++ build_files/install-packages.sh
@@ -85,7 +85,7 @@ BLUETOOTH_PACKAGES=(
     NetworkManager-bluetooth
     bluez
     gnome-bluetooth
-    # gnome-bluetooth-libs
+    gnome-bluetooth-libs
 )
 
 # centos specific packages
@@ -111,7 +111,7 @@ UNVANTED_PACKAGES=(
     NetworkManager-wwan
     gweather-locations
     gweather-locations-common
-    totem-pl-parser
+    # totem-pl-parser
 )
 
 # Guest Desktop Agents
