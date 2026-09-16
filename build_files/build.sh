41e2ad37adba1890a6565b6c809b95b5ec08591f
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-09-16 14:34:12 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,10 +17,10 @@ cp -avf "/ctx/system_files"/. /
 ### remove fedora flatpak remote
 #flatpak remote-delete fedora
 #flatpak remote-delete fedora-testing
-#systemctl disable flatpak-add-fedora-repos.service
+systemctl disable flatpak-add-fedora-repos.service
 
 ### add flathub repository
-# flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 ### install flatpack app
 
