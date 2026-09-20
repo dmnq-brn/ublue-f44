7e3ab7fb318ddb05806508d4c81fbcf863006c46
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-09-20 17:21:14 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,20 +13,23 @@ cp -avf "/ctx/system_files"/. /
 
 ### remove dnf repository
 # rm /etc/yum.repos.d/*.repo
+### remove gnome-initial-setup defaults configuration
+rm /usr/share/dconf/gnome-initial-setup
+rm /usr/share/gnome-initial-setup/initial-setup-dconf-defaults
+rm /usr/share/gnome-initial-setup/vendor.conf
 
-### remove fedora flatpak remote
+### remove fedora flatpak remote repository
 #flatpak remote-delete fedora
 #flatpak remote-delete fedora-testing
 systemctl disable flatpak-add-fedora-repos.service
 rm /usr/lib/systemd/system/flatpak-add-fedora-repos.service
 
-### add flathub repository
+### add flathub flatpak remote repository
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 ### install flatpack app
-
 flatpak install -y --noninteractive --system flathub com.mattjakeman.ExtensionManager
 flatpak install -y --noninteractive --system flathub page.tesk.Refine
 flatpak install -y --noninteractive --system flathub net.nokyan.Resources
-flatpak install -y --noninteractive --system flathub com.google.Chrome
+# flatpak install -y --noninteractive --system flathub com.google.Chrome
 
