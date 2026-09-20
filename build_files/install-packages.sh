b2b1ad4bb17cce80646d4582a8888c9ac4de7a91
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-09-20 12:18:53 +0200

    Update install-packages.sh

--- build_files/install-packages.sh
+++ build_files/install-packages.sh
@@ -182,6 +182,7 @@ UNVANTED_WORKSTATION_PACKAGES=(
 # Install required packages
 dnf -y --setopt=install_weak_deps=False install "${UNINSTALLED[@]}"
 # remove unwanted packages
+
 readarray -t INSTALLED < <(rpm -qa --queryformat='%{NAME}\n' "${UNVANTED_WORKSTATION_PACKAGES[@]}" 2>/dev/null || true)
 if [[ "${#INSTALLED[@]}" -gt 0 ]]; then
     dnf -y remove "${INSTALLED[@]}"
